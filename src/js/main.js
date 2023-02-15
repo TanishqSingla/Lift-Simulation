11db7d1f21693c2d9d5c3f96c23d0fc7525fa229
tanishqsingla <tanishqsingla00@gmail.com>
2023-02-16 02:19:35 +0530

    chore: Format code using prettier

--- src/js/main.js
+++ src/js/main.js
@@ -11,38 +11,45 @@ class Elevator {
     this.currentFloor = currentFloor;
     this.createDOMInstance();
   }
-  getState = () => (this.currentState);
-  getFloor = () => (this.currentFloor);
-  getElevatorDOMInstance = () => (this.elevatorDOMInstance);
-  setFloor = (floorIndex) => this.currentFloor = floorIndex;
-  setStatusActive = () => this.currentState = this.states[1];
-  setStatusIdle = () => this.currentState = this.states[0];
+  getState = () => this.currentState;
+  getFloor = () => this.currentFloor;
+  getElevatorDOMInstance = () => this.elevatorDOMInstance;
+  setFloor = (floorIndex) => (this.currentFloor = floorIndex);
+  setStatusActive = () => (this.currentState = this.states[1]);
+  setStatusIdle = () => (this.currentState = this.states[0]);
 
   createDOMInstance = () => {
-    this.elevatorDOMInstance = createDOMElementWithClass('div', 'elevator');
-    this.elevatorDoorDOMInstance = createDOMElementWithClass('div', 'elevator-door'); 
+    this.elevatorDOMInstance = createDOMElementWithClass("div", "elevator");
+    this.elevatorDoorDOMInstance = createDOMElementWithClass(
+      "div",
+      "elevator-door"
+    );
     this.elevatorDOMInstance.appendChild(this.elevatorDoorDOMInstance);
-  }
+  };
 
   moveToFloor(floorIndex) {
     this.setStatusActive();
     const floorTransitionDifference = Math.abs(this.currentFloor - floorIndex);
     const floorIndexOffset = this.maxFloors - floorIndex;
     this.currentFloor = floorIndex;
-    this.elevatorDOMInstance.style.transition = `${(floorTransitionDifference) * 500}ms ease-in-out`;
-    this.elevatorDOMInstance.style.transform = `translateY(-${(floorIndexOffset - 1) * floorHeight}px)`;
+    this.elevatorDOMInstance.style.transition = `${
+      floorTransitionDifference * 500
+    }ms ease-in-out`;
+    this.elevatorDOMInstance.style.transform = `translateY(-${
+      (floorIndexOffset - 1) * floorHeight
+    }px)`;
     setTimeout(() => {
       this.openDoors();
     }, 500 * floorTransitionDifference);
   }
 
   openDoors = async () => {
-    this.elevatorDoorDOMInstance.classList.add('open');
+    this.elevatorDoorDOMInstance.classList.add("open");
     setTimeout(() => {
-      this.elevatorDoorDOMInstance.classList.remove('open');
+      this.elevatorDoorDOMInstance.classList.remove("open");
       this.setStatusIdle();
-    }, 1500)
-  }
+    }, 1500);
+  };
 }
 
 class Floor {
@@ -57,24 +64,38 @@ class Floor {
     this.createFloorDOMInstance();
   }
   createFloorDOMInstance = () => {
-    this.floorDOMInstance = createDOMElementWithClass('div', 'floor');
-    const buttonGroup = createDOMElementWithClass('div', 'elevator-buttons');
-    if(this.floorIndex === 0) {
-      this.floorDownButton = createDOMElementWithClass('button', 'down-button', "Down");
+    this.floorDOMInstance = createDOMElementWithClass("div", "floor");
+    const buttonGroup = createDOMElementWithClass("div", "elevator-buttons");
+    if (this.floorIndex === 0) {
+      this.floorDownButton = createDOMElementWithClass(
+        "button",
+        "down-button",
+        "Down"
+      );
       buttonGroup.appendChild(this.floorDownButton);
-    } 
-    else if(this.floorIndex === this.maxFloors - 1) {
-      this.floorUpButton = createDOMElementWithClass('button', 'up-button', "Up");
+    } else if (this.floorIndex === this.maxFloors - 1) {
+      this.floorUpButton = createDOMElementWithClass(
+        "button",
+        "up-button",
+        "Up"
+      );
       buttonGroup.appendChild(this.floorUpButton);
-    }
-    else {
-      this.floorDownButton = createDOMElementWithClass('button', 'down-button', "Down");
-      this.floorUpButton = createDOMElementWithClass('button', 'up-button', "Up");
+    } else {
+      this.floorDownButton = createDOMElementWithClass(
+        "button",
+        "down-button",
+        "Down"
+      );
+      this.floorUpButton = createDOMElementWithClass(
+        "button",
+        "up-button",
+        "Up"
+      );
       buttonGroup.appendChild(this.floorUpButton);
       buttonGroup.appendChild(this.floorDownButton);
     }
     this.floorDOMInstance.appendChild(buttonGroup);
-  }
+  };
 
   getFloorDOMInstance = () => this.floorDOMInstance;
   getUpButton = () => this.floorUpButton;
@@ -82,56 +103,74 @@ class Floor {
   getFloorIndex = () => this.floorIndex;
 }
 
-class Building {;
+class Building {
   floors = [];
   elevators = [];
   buildingDOMInstance;
   constructor(floors, elevators) {
-    this.buildingDOMInstance = document.getElementById('building');
-    for(let i = 0; i < floors; i++) {
+    this.buildingDOMInstance = document.getElementById("building");
+    for (let i = 0; i < floors; i++) {
       this.floors.push(new Floor(i, floors));
       const floorUpButton = this.floors[i].getUpButton();
       const floorDownButton = this.floors[i].getDownButton();
-      if(floorUpButton) {
-        floorUpButton.addEventListener('click', () => this.callElevatorUp(i))
+      if (floorUpButton) {
+        floorUpButton.addEventListener("click", () => this.callElevatorUp(i));
       }
-      if(floorDownButton) {
-        floorDownButton.addEventListener('click', () => this.callElevatorDown(i))
+      if (floorDownButton) {
+        floorDownButton.addEventListener("click", () =>
+          this.callElevatorDown(i)
+        );
       }
     }
-    for(let i = 0; i < elevators; i++) {
-      this.elevators.push((new Elevator(floors, 0)))
+    for (let i = 0; i < elevators; i++) {
+      this.elevators.push(new Elevator(floors, 0));
     }
-    this.elevators.map(elevator => this.floors[floors - 1].getFloorDOMInstance().appendChild(elevator.getElevatorDOMInstance()));
-    this.elevators.forEach(elevator => elevator.setFloor(this.floors.length - 1));
+    this.elevators.map((elevator) =>
+      this.floors[floors - 1]
+        .getFloorDOMInstance()
+        .appendChild(elevator.getElevatorDOMInstance())
+    );
+    this.elevators.forEach((elevator) =>
+      elevator.setFloor(this.floors.length - 1)
+    );
 
-    this.floors.map(floor => this.buildingDOMInstance.appendChild(floor.getFloorDOMInstance()));
+    this.floors.map((floor) =>
+      this.buildingDOMInstance.appendChild(floor.getFloorDOMInstance())
+    );
     floorHeight = this.floors[0].floorDOMInstance.offsetHeight;
   }
 
   callElevatorUp(floorIndex) {
-    const isElevatorOnFloor = this.elevators.find(elevator => elevator.getFloor() === floorIndex);
-    if(!isElevatorOnFloor) {
-      const idleElevator = this.elevators.find(elevator => elevator.getState() === "idle");
+    const isElevatorOnFloor = this.elevators.find(
+      (elevator) => elevator.getFloor() === floorIndex
+    );
+    if (!isElevatorOnFloor) {
+      const idleElevator = this.elevators.find(
+        (elevator) => elevator.getState() === "idle"
+      );
       idleElevator.moveToFloor(floorIndex);
     } else {
       isElevatorOnFloor.openDoors();
     }
   }
   callElevatorDown(floorIndex) {
-    const isElevatorOnFloor = this.elevators.find(elevator => elevator.getFloor() === floorIndex);
-    if(!isElevatorOnFloor) {
-      const idleElevator = this.elevators.find(elevator => elevator.getState() === "idle");
+    const isElevatorOnFloor = this.elevators.find(
+      (elevator) => elevator.getFloor() === floorIndex
+    );
+    if (!isElevatorOnFloor) {
+      const idleElevator = this.elevators.find(
+        (elevator) => elevator.getState() === "idle"
+      );
       idleElevator.moveToFloor(floorIndex);
     } else {
-      isElevatorOnFloor.openDoors()
+      isElevatorOnFloor.openDoors();
     }
   }
 }
 
-const form = document.querySelector('form')
-const floorInput = document.getElementById('floors');
-const elevatorInput = document.getElementById('elevators');
+const form = document.querySelector("form");
+const floorInput = document.getElementById("floors");
+const elevatorInput = document.getElementById("elevators");
 let floorHeight;
 
 let maxFloors = 0;
@@ -142,17 +181,23 @@ function hanldeSubmitInputs(event) {
   const floors = +floorInput.value;
   const elevators = +elevatorInput.value;
 
-  if(!(floors && elevators)) {return;}
-  if(floors < 0 || floors > 5) {return;}
-  if(elevators < 0 || elevators > 5) {return;}
+  if (!(floors && elevators)) {
+    return;
+  }
+  if (floors < 0 || floors > 5) {
+    return;
+  }
+  if (elevators < 0 || elevators > 5) {
+    return;
+  }
 
   new Building(floors, elevators);
   form.style.display = "none";
 }
 
-function createDOMElementWithClass(element, className, text = '') {
+function createDOMElementWithClass(element, className, text = "") {
   const domElement = document.createElement(element);
   domElement.classList.add(className);
-  domElement.textContent = text
+  domElement.textContent = text;
   return domElement;
 }
