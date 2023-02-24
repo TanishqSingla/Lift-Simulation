202bfd2ebe1a7a58899f98bb44c78d2f8774bbc4
tanishqsingla <tanishqsingla00@gmail.com>
2023-02-24 19:06:22 +0530

    feat: Add typescript to project
         Add prettier to project
         Fix typescript file

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,42 +1,45 @@
-"use strict";
-var State;
-(function (State) {
-	State[(State["IDLE"] = 0)] = "IDLE";
-	State[(State["ACTIVE"] = 1)] = "ACTIVE";
-})(State || (State = {}));
+enum State {
+	IDLE,
+	ACTIVE,
+}
+
 const states = {
 	IDLE: 0,
 	ACTIVE: 1,
 };
+
 class Elevator {
-	constructor(maxFloors, currentFloor) {
-		this.getState = () => this.currentState;
-		this.getFloor = () => this.currentFloor;
-		this.getElevatorDOMInstance = () => this.elevatorDOMInstance;
-		this.getElevatorTimeout = () => this.elevatorTimeout;
-		this.setFloor = (floorIndex) => (this.currentFloor = floorIndex);
-		this.setStatusActive = () => (this.currentState = states.ACTIVE);
-		this.setStatusIdle = () => (this.currentState = states.IDLE);
-		this.openDoors = () => {
-			this.elevatorDoorDOMInstance.classList.add("open");
-			this.elevatorTimeout = setTimeout(() => {
-				this.elevatorDoorDOMInstance.classList.remove("open");
-				setTimeout(() => {
-					this.currentState = states.IDLE;
-				}, 2500);
-			}, 5000);
-		};
+	private currentState: State;
+	private currentFloor: number;
+	private maxFloors: number;
+	private elevatorDOMInstance: HTMLDivElement;
+	private elevatorDoorDOMInstance: HTMLDivElement;
+	private elevatorTimeout: ReturnType<typeof setTimeout> | undefined;
+
+	constructor(maxFloors: number, currentFloor: number) {
 		this.currentState = states.IDLE;
 		this.maxFloors = maxFloors;
 		this.currentFloor = currentFloor;
-		this.elevatorDOMInstance = createDOMElementWithClass("div", "elevator");
+		this.elevatorDOMInstance = createDOMElementWithClass(
+			"div",
+			"elevator"
+		) as HTMLDivElement;
 		this.elevatorDoorDOMInstance = createDOMElementWithClass(
 			"div",
 			"elevator-door"
-		);
+		) as HTMLDivElement;
 		this.elevatorDOMInstance.appendChild(this.elevatorDoorDOMInstance);
 	}
-	moveToFloor(floorIndex) {
+
+	getState = () => this.currentState;
+	getFloor = () => this.currentFloor;
+	getElevatorDOMInstance = () => this.elevatorDOMInstance;
+	getElevatorTimeout = () => this.elevatorTimeout;
+	setFloor = (floorIndex: number) => (this.currentFloor = floorIndex);
+	public setStatusActive = () => (this.currentState = states.ACTIVE);
+	public setStatusIdle = () => (this.currentState = states.IDLE);
+
+	moveToFloor(floorIndex: number) {
 		this.currentState = states.ACTIVE;
 		const totalTime = Math.abs(this.currentFloor - floorIndex) * 2000;
 		const floorIndexOffset = this.maxFloors - floorIndex - 1;
@@ -49,55 +52,83 @@ class Elevator {
 			this.openDoors();
 		}, totalTime);
 	}
+
+	openDoors = () => {
+		this.elevatorDoorDOMInstance.classList.add("open");
+		this.elevatorTimeout = setTimeout(() => {
+			this.elevatorDoorDOMInstance.classList.remove("open");
+			setTimeout(() => {
+				this.currentState = states.IDLE;
+			}, 2500);
+		}, 5000);
+	};
 }
+
 class Floor {
-	constructor(floorIndex, maxFloors) {
-		this.createFloorDOMInstance = () => {};
-		this.getFloorDOMInstance = () => this.floorDOMInstance;
-		this.getUpButton = () => this.floorUpButton;
-		this.getDownButton = () => this.floorDownButton;
-		this.getFloorIndex = () => this.floorIndex;
+	private floorIndex;
+	private floorDownButton: HTMLDivElement | undefined;
+	private floorUpButton: HTMLDivElement | undefined;
+	private floorDOMInstance: HTMLDivElement;
+	private maxFloors;
+	constructor(floorIndex: number, maxFloors: number) {
 		this.floorIndex = floorIndex;
 		this.maxFloors = maxFloors;
 		this.createFloorDOMInstance();
-		this.floorDOMInstance = createDOMElementWithClass("div", "floor");
-		const buttonGroup = createDOMElementWithClass("div", "elevator-buttons");
+		this.floorDOMInstance = createDOMElementWithClass(
+			"div",
+			"floor"
+		) as HTMLDivElement;
+		const buttonGroup = createDOMElementWithClass(
+			"div",
+			"elevator-buttons"
+		) as HTMLDivElement;
 		if (this.floorIndex === 0) {
 			this.floorDownButton = createDOMElementWithClass(
 				"button",
 				"down-button",
 				"Down"
-			);
+			) as HTMLDivElement;
 			buttonGroup.appendChild(this.floorDownButton);
 		} else if (this.floorIndex === this.maxFloors - 1) {
 			this.floorUpButton = createDOMElementWithClass(
 				"button",
 				"up-button",
 				"Up"
-			);
+			) as HTMLDivElement;
 			buttonGroup.appendChild(this.floorUpButton);
 		} else {
 			this.floorDownButton = createDOMElementWithClass(
 				"button",
 				"down-button",
 				"Down"
-			);
+			) as HTMLDivElement;
 			this.floorUpButton = createDOMElementWithClass(
 				"button",
 				"up-button",
 				"Up"
-			);
+			) as HTMLDivElement;
 			buttonGroup.appendChild(this.floorUpButton);
 			buttonGroup.appendChild(this.floorDownButton);
 		}
 		this.floorDOMInstance.appendChild(buttonGroup);
 	}
+	createFloorDOMInstance = () => {};
+
+	getFloorDOMInstance = () => this.floorDOMInstance;
+	getUpButton = () => this.floorUpButton;
+	getDownButton = () => this.floorDownButton;
+	getFloorIndex = () => this.floorIndex;
 }
+
 class Building {
-	constructor(floors, elevators) {
-		this.floors = [];
-		this.elevators = [];
-		this.buildingDOMInstance = document.getElementById("building");
+	private floors: Floor[] = [];
+	private elevators: Elevator[] = [];
+	private buildingDOMInstance;
+
+	constructor(floors: number, elevators: number) {
+		this.buildingDOMInstance = document.getElementById(
+			"building"
+		) as HTMLDivElement;
 		for (let i = 0; i < floors; i++) {
 			this.floors.push(new Floor(i, floors));
 			const floorUpButton = this.floors[i].getUpButton();
@@ -122,12 +153,14 @@ class Building {
 		this.elevators.forEach((elevator) =>
 			elevator.setFloor(this.floors.length - 1)
 		);
+
 		this.floors.map((floor) =>
 			this.buildingDOMInstance.appendChild(floor.getFloorDOMInstance())
 		);
 		floorHeight = this.floors[0].getFloorDOMInstance().offsetHeight;
 	}
-	callElevator(floorIndex) {
+
+	callElevator(floorIndex: number) {
 		const idleElevator = this.elevators.find(
 			(elevator) => elevator.getState() === states.IDLE
 		);
@@ -137,10 +170,12 @@ class Building {
 		}
 		return 1; // nope
 	}
-	requestElevator(floorIndex) {
+
+	requestElevator(floorIndex: number) {
 		const isElevatorOnFloor = this.elevators.find(
 			(elevator) => elevator.getFloor() === floorIndex
 		);
+
 		if (isElevatorOnFloor) {
 			isElevatorOnFloor.setStatusActive();
 			clearTimeout(isElevatorOnFloor.getElevatorTimeout());
@@ -154,16 +189,20 @@ class Building {
 		}
 	}
 }
-const form = document.querySelector("form");
-const floorInput = document.getElementById("floors");
-const elevatorInput = document.getElementById("elevators");
-let floorHeight;
+
+const form = document.querySelector("form") as HTMLFormElement;
+const floorInput = document.getElementById("floors") as HTMLInputElement;
+const elevatorInput = document.getElementById("elevators") as HTMLInputElement;
+let floorHeight: number;
+
 let maxFloors = 0;
 let lifts = 0;
-function hanldeSubmitInputs(event) {
+
+function hanldeSubmitInputs(event: SubmitEvent) {
 	event.preventDefault();
 	const floors = +floorInput.value;
 	const elevators = +elevatorInput.value;
+
 	if (!(floors && elevators)) {
 		return;
 	}
@@ -173,10 +212,16 @@ function hanldeSubmitInputs(event) {
 	if (elevators <= 0) {
 		return;
 	}
+
 	new Building(floors, elevators);
 	form.style.display = "none";
 }
-function createDOMElementWithClass(element, className, text = "") {
+
+function createDOMElementWithClass(
+	element: string,
+	className: string,
+	text = ""
+) {
 	const domElement = document.createElement(element);
 	domElement.classList.add(className);
 	domElement.textContent = text;
